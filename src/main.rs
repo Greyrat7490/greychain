eef935b4a54d0ba9a88b87746fcdaca72c345d5f
Greyrat7490 <kevinschaperjahn13@gmail.com>
2023-04-11 12:25:00 +0200

    update tests

--- src/main.rs
+++ src/main.rs
@@ -14,45 +14,35 @@ extern crate rand;
 extern crate digest;
 
 fn main() {
-    let wallet1 = Wallet::new(&vec![]);
+    const WALLETS_COUNT: usize = 3;
+    const TXS_PER_WALLET: usize = 3;
 
-    let master_nodes = vec![Node{ pub_key: wallet1.pub_key_pem.clone(), port: wallet1.port, online: true}];
+    let wallets = create_test_wallets(WALLETS_COUNT);
 
-    let wallet2 = Wallet::new(&master_nodes);
-    let wallet3 = Wallet::new(&master_nodes);
+    create_txs(&wallets, TXS_PER_WALLET);
 
-    wallet1.send_tx(&wallet2.pub_key_pem, 420.69);
-    wallet1.send_tx(&wallet3.pub_key_pem, 420.69);
+    wait_for_wallets(&wallets);
 
-    wallet2.send_tx(&wallet1.pub_key_pem, 69.64);
-    wallet2.send_tx(&wallet3.pub_key_pem, 69.64);
-    
-    wallet3.send_tx(&wallet1.pub_key_pem, 64.420);
-    wallet3.send_tx(&wallet2.pub_key_pem, 64.420);
+    let txs = wallets[0].get_tx_ids();
+    let hashes = wallets.iter().map(|w| w.get_cur_hash()).collect::<Vec<u64>>();
+    let net_lens = wallets.iter().map(|w| w.get_network_len()).collect::<Vec<usize>>();
 
-    wallet1.show_network();
-    wallet2.show_network();
-    wallet3.show_network();
+    shutdown_test_wallets(wallets);
 
-    while !wallet1.is_idling() || !wallet2.is_idling() || !wallet3.is_idling() { 
-        sleep(Duration::from_millis(100));
-    }     
-
-    let txs = wallet1.get_tx_ids();
-
-    wallet1.shutdown();
-    wallet2.shutdown();
-    wallet3.shutdown();
-
+    println!("\n-------------------"); 
     println!("pkgs total send: {}", get_pkgs_send()); 
+    println!("net_lens: {:?}", net_lens);
+    println!("hashes:\n{}", hashes.iter().map(|h| {h.to_string() + "\n"}).collect::<String>());
     println!("txs: {:?}", txs); 
+    println!("txs count: {:?}", txs.len()); 
+    println!("-------------------"); 
 }
 
 #[cfg(test)]
 mod tests {
     use std::{time::Duration, thread::sleep};
 
-    use crate::{wallet::Wallet, net::node::Node};
+    use crate::{wait_for_wallets, create_test_wallets, shutdown_test_wallets, create_txs};
 
     #[test]
     fn network_3wallets() {
@@ -80,13 +70,13 @@ mod tests {
     }
 
     #[test]
-    fn no_missing_txs_3wallet_2tx() {
-        no_missing_txs(3, 2);
+    fn txs_3wallet_2tx() {
+        check_txs(3, 2);
     }
 
     #[test]
-    fn no_missing_txs_4wallet_4tx() {
-        no_missing_txs(4, 4);
+    fn txs_4wallet_4tx() {
+        check_txs(4, 4);
     }
 
 
@@ -132,60 +122,67 @@ mod tests {
         shutdown_test_wallets(wallets);
     }
 
-    fn no_missing_txs(wallets_count: usize, txs_count: usize) {
+    fn check_txs(wallets_count: usize, pre_wallet_txs_count: usize) {
         let wallets = create_test_wallets(wallets_count);
 
-        create_txs(&wallets, txs_count);
+        create_txs(&wallets, pre_wallet_txs_count);
 
         wait_for_wallets(&wallets);
 
+        let txs_total = (pre_wallet_txs_count*wallets_count).try_into().expect("cound not cast u64 into usize");
+        let expected = (0..txs_total).collect::<Vec<u64>>();
         for wallet in &wallets {
-            assert_eq!(wallet.get_tx_ids().len(), txs_count);
+            let mut res = wallet.get_tx_ids();
+            res.sort_unstable();
+
+            // tx ids will not always start from 0 (global id counter)
+            let offset = *res.first().expect("expected at least one tx");
+            res = res.iter_mut().map(|id| *id - offset).collect();
+
+            assert_eq!(res, expected);
         }
 
         shutdown_test_wallets(wallets);
-    }
+    }       
+}
 
-        
-    fn create_test_wallets(wallets_count: usize) -> Vec<Wallet> {
-        let mut wallets = Vec::<Wallet>::with_capacity(wallets_count);
-        wallets.push(Wallet::new(&vec![]));
+fn create_test_wallets(wallets_count: usize) -> Vec<Wallet> {
+    let mut wallets = Vec::<Wallet>::with_capacity(wallets_count);
+    wallets.push(Wallet::new(&vec![]));
 
-        let master_nodes = vec![Node{ pub_key: wallets[0].pub_key_pem.clone(), port: wallets[0].port, online: true}];
-        wallets.resize_with(wallets_count, || { Wallet::new(&master_nodes) });
+    let master_nodes = vec![Node{ pub_key: wallets[0].pub_key_pem.clone(), port: wallets[0].port, online: true}];
+    wallets.resize_with(wallets_count, || { Wallet::new(&master_nodes) });
 
-        return wallets;
-    }
+    return wallets;
+}
 
-    fn create_txs(wallets: &Vec<Wallet>, txs_count: usize) {
-        for i in 0..wallets.len() {
-            for mut j in 0..txs_count {
-                if j == i { j += 1; }
-                let idx = j % wallets.len();
+fn create_txs(wallets: &Vec<Wallet>, txs_count: usize) {
+    for i in 0..wallets.len() {
+        for mut j in 0..txs_count {
+            if j == i { j += 1; }
+            let idx = j % wallets.len();
 
-                wallets[i].send_tx(&wallets[idx].pub_key_pem, rand::random::<f64>() * 100.0);
-            }
+            wallets[i].send_tx(&wallets[idx].pub_key_pem, rand::random::<f64>() * 100.0);
         }
     }
+}
 
-    fn shutdown_test_wallets(wallets: Vec<Wallet>) {
+fn shutdown_test_wallets(wallets: Vec<Wallet>) {
+    for wallet in wallets {
+        wallet.shutdown();
+    }
+}
+
+fn wait_for_wallets(wallets: &Vec<Wallet>) {
+    loop {
+        let mut idling = false;
         for wallet in wallets {
-            wallet.shutdown();
+            idling = wallet.is_idling();
+            if !idling { break; }
         }
-    }
 
-    fn wait_for_wallets(wallets: &Vec<Wallet>) {
-        // wait until all wallets are idling
-        loop {
-            let mut idling = false;
-            for wallet in wallets {
-                idling = wallet.is_idling();
-                if !idling { break; }
-            }
+        if idling { break; }
 
-            if idling { break; }
-
-            sleep(Duration::from_millis(100));
-        }
+        sleep(Duration::from_millis(100));
     }
 }
